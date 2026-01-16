47119dbc6b61686490a4e696ed8cab1a1533597b
Sowmya-id26 <163761384+Sowmya-id26@users.noreply.github.com>
2026-01-16 22:56:29 +0530

    Delete Question-2/math.js

--- Question-2/math.js
+++
@@ -1,7 +0,0 @@
-import { checkPrime}  from "./index.js";
-
-console.log(checkPrime(1));
-console.log(checkPrime(2));
-console.log(checkPrime(54));
-console.log(checkPrime(78));
-console.log(checkPrime(7));
